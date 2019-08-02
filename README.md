40bd69e8a8c0e9cb71439505f48c2e071e6e523a
Mike <msummers@newrelic.com>
2019-08-02 17:15:02 -0500

    Fighting with Infra

---
+++ README.md
@@ -0,0 +1,9 @@
+# PM2 OHI
+Stateless Nodejs OHI that reports metrics from PM2 managed applications.
+
+## Installation
+- `npm install`
+
+## Configuration
+
+
--- pm2ohi.js
+++ pm2ohi.js
@@ -8,10 +8,10 @@ var duration = 30;
 // Running restart
 var restartList = {};
 
-function bytesToMB (bytes, precision) {
+function bytesToMB(bytes, precision) {
     var kilobyte = 1024;
     var megabyte = kilobyte * 1024;
-    return parseFloat((bytes / megabyte).toFixed(precision)) ;
+    return parseFloat((bytes / megabyte).toFixed(precision));
 };
 
 function secondsSince(date) {
@@ -34,14 +34,14 @@ pmx.initModule({}, function (err, conf) {
                 process.exit(1);
             }
 
+            var processCount = 0;
             var msg = {};
             msg.name = "com.newrelic.pm2";
-            msg.protocol_version = "2";
+            msg.protocol_version = "3";
             msg.integration_version = "1.0.0";
 
             var data = [];
             msg.data = data;
-            var processCount = 0;
             processes.forEach(function (process) {
                 data[processCount] = {};
                 var entity = {};
@@ -50,32 +50,34 @@ pmx.initModule({}, function (err, conf) {
                 entity.type = "com.newrelic.pm2.process";
                 entity.id_attributes = [];
                 var i = 0;
-                entity.id_attributes[i++] = {"pm_id": process.pm_id};
-                entity.id_attributes[i++] = {"version": process.pm2_env.version};
-                entity.id_attributes[i++] = {"execMode": process.pm2_env.exec_mode};
-                entity.id_attributes[i++] = {"user": process.pm2_env.username};
+                entity.id_attributes[i++] = {"key": "pm_id", "value": process.pm_id};
+                entity.id_attributes[i++] = {"key": "version", "value": process.pm2_env.version};
+                entity.id_attributes[i++] = {"key": "execMode", "value": process.pm2_env.exec_mode};
+                entity.id_attributes[i++] = {"key": "user", "value": process.pm2_env.username};
 
                 var inventory = {}
                 data[processCount].inventory = inventory;
-                inventory["name"] = process.name;
-                inventory["autorestart"] = process.pm2_env.autorestart;
-                inventory["execMode"] = process.pm2_env.exec_mode;
-                inventory["execInterpreter"] = process.pm2_env.exec_interpreter;
-                inventory["execPath"] = process.pm2_env.pm_exec_path;
-                inventory["pmCWD"] = process.pm2_env.pm_cwd;
-                inventory["instances"] = process.pm2_env.instances;
-                inventory["nodeArgs"] = process.pm2_env.node_args;
-                inventory["pmOutLogPath"] = process.pm2_env.pm_out_log_path;
-                inventory["pmErrLogPath"] = process.pm2_env.pm_err_log_path;
-                inventory["pmPidPath"] = process.pm2_env.pm_pid_path;
-                inventory["vizionRunning"] = process.pm2_env.vizion_running;
-                inventory["createdAt"] = process.pm2_env.created_at;
-                inventory["pmID"] = process.pm2_env.pm_id;
-                inventory["startedInside"] = process.pm2_env.started_inside;
-                inventory["command"] = process.pm2_env.command;
-                inventory["versioning"] = process.pm2_env.versioning;
-                inventory["version"] =process.pm2_env.version;
-                inventory["username"] =process.pm2_env.username;
+                var processSettings = {};
+                 processSettings["name"] = process.name;
+                 processSettings["autorestart"] = process.pm2_env.autorestart;
+                 processSettings["execMode"] = process.pm2_env.exec_mode;
+                 processSettings["execInterpreter"] = process.pm2_env.exec_interpreter;
+                 processSettings["execPath"] = process.pm2_env.pm_exec_path;
+                 processSettings["pmCWD"] = process.pm2_env.pm_cwd;
+                 processSettings["instances"] = process.pm2_env.instances;
+                 //processSettings["nodeArgs"] = process.pm2_env.node_args;
+                 processSettings["pmOutLogPath"] = process.pm2_env.pm_out_log_path;
+                 processSettings["pmErrLogPath"] = process.pm2_env.pm_err_log_path;
+                 processSettings["pmPidPath"] = process.pm2_env.pm_pid_path;
+                 processSettings["vizionRunning"] = process.pm2_env.vizion_running;
+                 processSettings["createdAt"] = process.pm2_env.created_at;
+                 processSettings["pmID"] = process.pm2_env.pm_id;
+                 processSettings["startedInside"] = process.pm2_env.started_inside;
+                 //processSettings["command"] = process.pm2_env.command;
+                 //processSettings["versioning"] = process.pm2_env.versioning;
+                 processSettings["version"] =process.pm2_env.version;
+                 processSettings["username"] =process.pm2_env.username;
+                 inventory["processSettings"] = processSettings;
 
 
                 var events = []
@@ -89,16 +91,19 @@ pmx.initModule({}, function (err, conf) {
                 metrics[0]["eventType"] = "PM2MetricsSample";
                 metrics[0]["entity_name"] = entity.type + ":" + entity.name;
                 metrics[0]["processUptimeSeconds"] = secondsSince(process.pm2_env.pm_uptime);
-                metrics[0]["processInstances"]=process.pm2_env.instances
-                metrics[0]["processStatus"]=process.pm2_env.status
-                metrics[0]["processUnstableRestarts"]=process.pm2_env.unstable_restarts
+                metrics[0]["processInstances"] = process.pm2_env.instances
+                metrics[0]["processStatus"] = process.pm2_env.status
+                metrics[0]["processUnstableRestarts"] = process.pm2_env.unstable_restarts
                 metrics[0]["processTotalRestarts"] = process.pm2_env.restart_time;
                 metrics[0]["processCpuPercent"] = process.monit.cpu;
                 metrics[0]["processMemoryMB"] = bytesToMB(process.monit.memory, 1);
+                processCount++;
             });
             var msgString = JSON.stringify(msg);
-            console.log(msgString);
-            process.exit();
+            process.stdout.write(msgString, () => {
+                console.error("Flushed");
+                process.exit(0);
+            });
         });
     });
 })
